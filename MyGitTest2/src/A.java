c7adfe198a63068ffa063a81b8a477c98f85e621
하 윤서 <hayunseo14@gmail.com>
2024-12-29 01:10:49 +0900

    A, B Changed

--- MyGitTest2/src/A.java
+++ MyGitTest2/src/A.java
@@ -4,7 +4,7 @@ public class A {
 		System.out.println("This is the 2nd version.");
 		System.out.println("My changed in master branch");
 		B your = new B();
-		your.sayHello("Everyone");
+		your.sayHello22("Everyone");
 		C your2 = new C();
 		your2.sayBye("Everyone");
 	}
--- MyGitTest2/src/B.java
+++ MyGitTest2/src/B.java
@@ -1,5 +1,5 @@
 public class B {
-	public void sayHello(String toWhom) {
+	public void sayHello22(String toWhom) {
 		System.out.println("Hello " + toWhom);
 	}
 }
